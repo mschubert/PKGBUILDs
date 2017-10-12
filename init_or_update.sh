98b0f8af1bda50dbc1490bdab66e5981aa94108c
mschubert <mschu.dev@gmail.com>
2017-10-12 12:03:03 +0200

    add user var

--- init_or_update.sh
+++ init_or_update.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 set -x
 
+# the AUR user name from whom to fetch the packages from
+user=mschu
+
 git stash
 
-AUR="https://aur.archlinux.org/rpc/?v=5&type=search&by=maintainer&arg=mschu"
+AUR="https://aur.archlinux.org/rpc/?v=5&type=search&by=maintainer&arg=$user"
 PKGs=$(curl -s -G "$AUR" | jq -r '.results[] | .Name' | sort)
 
 for PKG in $PKGs; do
