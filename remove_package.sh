f2515f9fa8a026e5f7dcea17af52964f4d0e536c
mschubert <mschu.dev@gmail.com>
2019-01-03 14:39:33 +0100

    confirm package removal

--- remove_package.sh
+++ remove_package.sh
@@ -5,9 +5,12 @@ cd $(dirname $0)
 
 # AUR packages are submitted via clone hook
 for pkg in $(echo $@ | sed 's:/$::'); do
-    git remote rm $pkg
-    git rm -rf $pkg && rm -rf $pkg
-    git commit -m "remove $pkg"
+    read -r -p "Remove $pkg? [y/N] " response
+    if [[ "$response" =~ ^([yY][eE][sS]|[yY])+$ ]]; then
+        git remote rm $pkg
+        git rm -rf $pkg && rm -rf $pkg
+        git commit -m "remove $pkg"
+    fi
 done
 
 git push
