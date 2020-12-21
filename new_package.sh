0b954eb8effe5b867f805847b6101dce9aab4c28
mschubert <mschu.dev@gmail.com>
2020-12-21 18:20:32 +0100

    fetch no longer req'd

--- new_package.sh
+++ new_package.sh
@@ -10,7 +10,6 @@ for pkg in $(echo $@ | sed 's:/$::'); do
     git clone ssh://aur@aur.archlinux.org/$pkg.git .temp_pkg
     rm -rf .temp_pkg
     git remote add $pkg ssh://aur@aur.archlinux.org/$pkg.git
-    git fetch $pkg
     git subtree add --prefix=$pkg $pkg master
     mkdir -p $pkg
 done
