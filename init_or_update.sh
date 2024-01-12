2a28993a8646014820cfc39fb9e74b64711f6fb3
Michael Schubert <mchu.dev@gmail.com>
2024-01-12 10:57:44 +0100

    allow init from command-line args

--- init_or_update.sh
+++ init_or_update.sh
@@ -6,8 +6,15 @@ user=mschu
 
 git stash
 
-AUR="https://aur.archlinux.org/rpc/?v=5&type=search&by=maintainer&arg=$user"
-PKGs=$(curl -s -G "$AUR" | jq -r '.results[] | .Name' | sort)
+# get PKGs from command line args or else the AUR
+PKGs=$(echo $@ | sed 's:/$::')
+if [[ -z $PKGs ]]; then
+    read -r -p "Command-line args for package is empty, pull from AUR? [y/N] " response
+    if [[ "$response" =~ ^([yY][eE][sS]|[yY])+$ ]]; then
+        AUR="https://aur.archlinux.org/rpc/?v=5&type=search&by=maintainer&arg=$user"
+        PKGs=$(curl -s -G "$AUR" | jq -r '.results[] | .Name' | sort)
+    fi
+fi
 
 for PKG in $PKGs; do
     git remote add $PKG ssh://aur@aur.archlinux.org/$PKG.git
